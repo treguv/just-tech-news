27fafcbdd370583bd23036cf3dbbec7e25ed1460
treguv <vladislavtregubov00@gmail.com>
2020-12-18 10:11:28 -0800

    Added sequilize variable declaration

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,11 @@ const { sequelize } = require("../models/Comment");
 
 require("dotenv").config(); // For loading env variables
 //create the connection to the database
-
+let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  const sequelize = new Sequelize(
+  sequelize = new Sequelize(
     process.env.DB_NAME, //vars set up in .env
     process.env.DB_USER,
     process.env.DB_PW,
